394114e8c088fc6688bd1427e605b67ebbbbbf9c
Medha Bhat <medha2512@gmail.com>
2014-08-11 23:43:42 -0700

    second commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   
-  config.action_mailer.default_url_options = { :host => 'online-survey-123.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'onlinecareersurvey.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
